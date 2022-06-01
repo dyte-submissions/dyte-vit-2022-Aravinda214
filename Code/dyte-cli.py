c086dbd46c313bf1d3e4daef4b9769fb1bd7e7c2
Aravinda <68860525+Aravinda214@users.noreply.github.com>
2022-06-02 04:01:15 +0530

    Update dyte-cli.py

--- Code/dyte-cli.py
+++ Code/dyte-cli.py
@@ -36,6 +36,63 @@ def version_check(gitLink,package,version):
     else:
         return False
 
+def get_version(gitLink,package,version):
+    user_repo = gitLink.split('/')[3:]
+    username = user_repo[0]
+    repository_name = user_repo[1]
+    file_path = 'package.json'
+
+    file_content = github_read_file(username, repository_name, file_path)
+    data = json.loads(file_content)
+    #print(data)
+    #print(type(data))
+
+    PackVer = data['dependencies'][package][1:]
+    return PackVer
+
+
+def repo_PR(gitLink,version_satisfied):
+    if version_satisfied:
+        return
+
+    user_repo = gitLink.split('/')[3:]
+    username = user_repo[0]
+    repository_name = user_repo[1]
+    file_path = 'package.json'
+
+    token = "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"
+
+    headers = {
+    "Authorization" : "token "+token,
+    "Accept" : "application/vnd.github.v3+json"
+    }
+
+    url = "https://api.github.com/repos/{}/{}/forks".format(username,repository_name)
+    response = requests.post(url,headers=headers)
+
+    newrepo = response.text['full_name']
+
+    from github import Github
+
+    token = "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"
+
+    api = Github(token)
+    site = api.get_repo('Aravinda214/'+repository_name)
+
+    index_file = site.get_contents('package.json')
+    index_content = base64.b64decode(index_file.content).decode("utf-8") 
+
+    updated_content = index_content.replace('"axios": "^0.21.1"','"axios": "^0.23.0",')
+
+    print(site.update_file(
+        path='package.json',
+        message='Updated Dependencies',
+        content=updated_content,
+        sha=index_file.sha
+    ))
+
+
+
 def main():
     parser = argparse.ArgumentParser(description='SDK Tooling Challenge - Node Packages Version Check')
     parser.add_argument('-i','--input', metavar='FILE', type=str, help="CSV of File Repos", required=True)
@@ -46,8 +103,13 @@ def main():
     package_ver = args.PACKAGE.split('@')
 
     df = pd.read_csv(args.input)
+    df['version'] = df.apply(lambda row: get_version(row.repo,package_ver[0],package_ver[1]), axis=1)
     df['version_satisfied'] = df.apply(lambda row: version_check(row.repo,package_ver[0],package_ver[1]), axis=1)
     print(df)
+
+
+    # df.apply(lambda row: repo_PR(row.repo,row.version_satisfied), axis=1)
+
 
     filename = 'output_' + args.input
     df.to_csv(filename,index=False)
@@ -55,5 +117,7 @@ def main():
     print("\n\nOutput saved as",filename)
 
 
-if __name__ == '__main__':
+
+
+if _name_ == '_main_':
     main()
